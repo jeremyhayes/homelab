4b7d1715b849c118eb225d48ce7156f8da0772a8
Jeremy Hayes <dev@jeremyhayes.net>
2021-10-11 20:24:40 -0400

    (wip) testing carrier infinity proxy

---
+++ carrier-infinity/homegrown/go.mod
@@ -0,0 +1,4 @@
+module jeremyhayes.net/logging-proxy
+
+go 1.17
+
---
+++ carrier-infinity/homegrown/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// setup handler for inbooud requests
+	http.HandleFunc("/", handle)
+        log.Print("starting up on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func copy(body io.ReadCloser) (stream io.ReadCloser, text *string) {
+	if body == nil {
+		return body, nil
+	}
+
+	buffer, _ := ioutil.ReadAll(body)
+	// close original body before replacing with copy
+	body.Close()
+	body = ioutil.NopCloser(bytes.NewBuffer(buffer))
+
+	copy := new(bytes.Buffer)
+	copy.Write(buffer)
+	result := copy.String()
+	return body, &result
+}
+
+func handle(res http.ResponseWriter, req *http.Request) {
+	// log request
+	log.Printf("--> %v", req.URL)
+	for k, v := range req.Header {
+		log.Printf("--> %v: %v", k, v)
+	}
+//	reqBodyCopy, reqBodyString := copy(req.Body)
+//	req.Body = reqBodyCopy
+//	if reqBodyString != nil {
+//		log.Printf("--> \n%v", *reqBodyString)
+//	}
+
+	// build proxy request
+	preq := http.Request{
+		Method:        req.Method,
+		URL:           req.URL,
+		Header:        req.Header,
+		Body:          req.Body,
+		ContentLength: req.ContentLength,
+		Close:         req.Close,
+	}
+
+	// forward to destination
+	pres, _ := http.DefaultTransport.RoundTrip(&preq)
+//	defer pres.Body.Close()
+
+	// log response
+        log.Printf("<-- %v", pres.Status)
+//	resBodyCopy, resBodyString := copy(pres.Body)
+//	pres.Body = resBodyCopy
+
+	// copy proxy response
+	for k, v := range pres.Header {
+		res.Header()[k] = v
+		log.Printf("<-- %v: %v", k, v)
+	}
+
+	// have to do this after headers, but before body
+	res.WriteHeader(pres.StatusCode)
+
+	// temp because fake endpoint doesn't let me set headers
+	if pres.ContentLength > 0 {
+		io.CopyN(res, pres.Body, pres.ContentLength)
+	} else if pres.Close {
+		for {
+			_, err := io.Copy(res, pres.Body)
+			if err != nil {
+				break
+			}
+		}
+	}
+
+//	if resBodyString != nil {
+//		log.Printf("<-- \n%v", *resBodyString)
+//	}
+}
+
