b2fdeace65ff83b6256f92d9ee5a233fcd37c13a
Jeremy Hayes <dev@jeremyhayes.net>
2021-10-15 19:41:57 -0400

    wip still trying

--- carrier-infinity/homegrown/main.go
+++ carrier-infinity/homegrown/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -33,19 +34,26 @@ func copy(body io.ReadCloser) (stream io.ReadCloser, text *string) {
 
 func handle(res http.ResponseWriter, req *http.Request) {
 	// log request
-	log.Printf("--> %v", req.URL)
+//        fmt.Println("logging the whole thing")
+	fmt.Printf("the whole thing %+v\n", req)
+	log.Printf("--> %v %v", req.Method, req.URL)
 	for k, v := range req.Header {
 		log.Printf("--> %v: %v", k, v)
 	}
-//	reqBodyCopy, reqBodyString := copy(req.Body)
-//	req.Body = reqBodyCopy
-//	if reqBodyString != nil {
-//		log.Printf("--> \n%v", *reqBodyString)
-//	}
+	reqBodyCopy, reqBodyString := copy(req.Body)
+	req.Body = reqBodyCopy
+	if reqBodyString != nil {
+		log.Printf("--> \n%v", *reqBodyString)
+	}
+
+//	fmt.Println("here's the whole request url...")
+//	fmt.Printf("request.URL: %+v\n", req.URL)
+//	fmt.Printf("IsAbs: %v", req.URL.IsAbs())
 
 	// build proxy request
 	preq := http.Request{
 		Method:        req.Method,
+//		URL:           "http://www.api.ing.carrier.com" + req.URL,
 		URL:           req.URL,
 		Header:        req.Header,
 		Body:          req.Body,
@@ -53,14 +61,21 @@ func handle(res http.ResponseWriter, req *http.Request) {
 		Close:         req.Close,
 	}
 
+	preq.URL.Scheme = "http"
+	preq.URL.Host = "www.api.ing.carrier.com"
+
 	// forward to destination
-	pres, _ := http.DefaultTransport.RoundTrip(&preq)
+	pres, err := http.DefaultTransport.RoundTrip(&preq)
 //	defer pres.Body.Close()
+        if (err != nil) {
+		log.Print("something bit me!!");
+		log.Print(err);
+	}
 
 	// log response
         log.Printf("<-- %v", pres.Status)
-//	resBodyCopy, resBodyString := copy(pres.Body)
-//	pres.Body = resBodyCopy
+	resBodyCopy, resBodyString := copy(pres.Body)
+	pres.Body = resBodyCopy
 
 	// copy proxy response
 	for k, v := range pres.Header {
@@ -83,8 +98,8 @@ func handle(res http.ResponseWriter, req *http.Request) {
 		}
 	}
 
-//	if resBodyString != nil {
-//		log.Printf("<-- \n%v", *resBodyString)
-//	}
+	if resBodyString != nil {
+		log.Printf("<-- \n%v", *resBodyString)
+	}
 }
 
