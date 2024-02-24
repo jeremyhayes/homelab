8ac1d517516231a6d33f34e9a66ed8edc057a64a
Jeremy Hayes <jeremyhayes@users.noreply.github.com>
2024-02-24 02:44:30 +0000

    remove no-longer-needed script for node arch label

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# set label for node architecture
-ARCH=$(docker info -f '{{.Architecture}}')
-NODE_ID=$(docker node inspect self -f '{{.ID}}')
-docker node update --label-add arch="$ARCH" "$NODE_ID"
