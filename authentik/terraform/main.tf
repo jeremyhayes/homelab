6dca46c547c8e73e311a9e71e10ca0ba7f220cb1
Jeremy Hayes <jeremyhayes@users.noreply.github.com>
2024-07-08 19:57:51 +0000

    move grafana oauth2 to tofu

--- authentik/terraform/main.tf
+++ authentik/terraform/main.tf
@@ -72,5 +72,71 @@ import {
 
 resource "authentik_outpost" "embedded" {
   name               = "authentik Embedded Outpost"
-  protocol_providers = [for k, v in authentik_provider_proxy.forward_auth_app_proxy : v.id]
+  protocol_providers = [
+    for k, v in authentik_provider_proxy.forward_auth_app_proxy : v.id
+  ]
+}
+
+data "authentik_certificate_key_pair" "self_signed" {
+  name              = "authentik Self-signed Certificate"
+}
+
+data "authentik_scope_mapping" "oauth2_scope_email" {
+  managed = "goauthentik.io/providers/oauth2/scope-email"
+}
+
+data "authentik_scope_mapping" "oauth2_scope_openid" {
+  managed = "goauthentik.io/providers/oauth2/scope-openid"
+}
+
+data "authentik_scope_mapping" "oauth2_scope_profile" {
+  managed = "goauthentik.io/providers/oauth2/scope-profile"
+}
+
+# define oauth2 applications
+locals {
+  oauth2_apps = {
+    grafana = {
+      name          = "Grafana"
+      client_id     = "Ix5MhtsNuENrzpqqPGBlIunfQpzcFdSGR1eyHB1r"
+      redirect_uris = [
+        "https://grafana.lab.omglolwtfbbq.com/login/generic_oauth"
+      ]
+      icon          = "grafana.svg"
+      launch_url    = "https://grafana.lab.omglolwtfbbq.com/login/generic_oauth"
+    }
+  }
+}
+
+# create oauth2 applications with associated provider
+resource "authentik_provider_oauth2" "oauth2_app_provider" {
+  for_each = local.oauth2_apps
+
+  name                  = format("Provider for %s", each.value.name)
+  authorization_flow    = data.authentik_flow.implicit_consent.id
+  client_id             = each.value.client_id
+  redirect_uris         = each.value.redirect_uris
+  sub_mode              = "user_username"
+  signing_key           = data.authentik_certificate_key_pair.self_signed.id
+  access_token_validity = "minutes=5"
+  property_mappings     = [
+    data.authentik_scope_mapping.oauth2_scope_openid.id,
+    data.authentik_scope_mapping.oauth2_scope_email.id,
+    data.authentik_scope_mapping.oauth2_scope_profile.id,
+  ]
+}
+
+resource "authentik_application" "oauth2_app" {
+  for_each = authentik_provider_oauth2.oauth2_app_provider
+
+  slug              = each.key
+  name              = local.oauth2_apps[each.key].name
+  protocol_provider = each.value.id
+
+  meta_icon         = local.oauth2_apps[each.key].icon != null ? (
+      format("application-icons/%s", local.oauth2_apps[each.key].icon)
+    ) : (
+      null
+    )
+  meta_launch_url   = local.oauth2_apps[each.key].launch_url
 }
